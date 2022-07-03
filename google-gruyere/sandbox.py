08c4d1010861594952efff63aadd08faf4bf7381
Yazan Lpizra <ylpizra@my.bridgeport.edu>
2022-07-02 23:41:58 -0400

    more gruyere tests

--- google-gruyere/sandbox.py
+++ google-gruyere/sandbox.py
@@ -1,6 +1,6 @@
 import re
 from gruyere_code.sanitize import SanitizeHtml
 
-html_snippet = '<input <script>alert()</script>'
+html_snippet = '<p <script>alert(1)</script>hello'
 
 print(SanitizeHtml(html_snippet))
